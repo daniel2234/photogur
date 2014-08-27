f1307bac535bd0f354189386d19bdd437908b8e5
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-08-27 18:33:25 -0400

    Making each picture show at its own URL using the static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
  get 'pictures' => 'pictures#index'
+
+ get 'pictures/0'=> 'pictures#picture0'
+ get 'pictures/1'=> 'pictures#picture1'
+ get 'pictures/2'=> 'pictures#picture2'
 end
