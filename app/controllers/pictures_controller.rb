e89b49c2451c494a9ff56060487ae3b4e1d3c6de
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-08-27 18:44:27 -0400

    Making wach picture show at its own URL using static routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,26 @@
 class PicturesController < ApplicationController
 	def index
 		@pictures = [
+	  {
+	    :title  => "The old church on the coast of White sea",
+	    :artist => "Sergey Ershov",
+	    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+	  },
+	  {
+	    :title  => "Sea Power",
+	    :artist => "Stephen Scullion",
+	    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+	  },
+	  {
+	    :title  => "Into the Poppies",
+	    :artist => "John Wilhelm",
+	    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+	  }
+	]
+	end
+
+	def show
+		@pictures = [
       {
         :title  => "The old church on the coast of White sea",
         :artist => "Sergey Ershov",
@@ -17,28 +37,6 @@ class PicturesController < ApplicationController
         :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
       }
     ]
-  end
-		def picture0
-		@picture = {
-		  :title  => "The old church on the coast of White sea",
-		  :artist => "Sergey Ershov",
-		  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-		}
-			end
-
-			def picture1
-		@picture = {
-		  :title  => "Sea Power",
-		  :artist => "Stephen Scullion",
-		  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-		}
-		end
-
-		def picture2
-		@picture = {
-		  :title  => "Into the Poppies",
-		  :artist => "John Wilhelm",
-		  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-		}
-		end
+    @picture = @pictures[params[:id].to_i]
+	end
 end
